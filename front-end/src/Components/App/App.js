2ff81e03a62c2091efbc1669cf9dd8200984bf74
Denis <deniskatarasenko6@gmail.com>
2023-04-17 21:05:31 +0300

    another boring commit

--- front-end/src/Components/App/App.js
+++ front-end/src/Components/App/App.js
@@ -8,7 +8,7 @@ import Main from "../Main/Main";
 import Login from "../Login/Login";
 import SignUp from "../SignUp/SignUp";
 
-const apiUrl = "https://dislord-chat-app.onrender.com/";
+const apiUrl = "https://dislord-chat-app.onrender.com";
 
 const socket = io.connect(apiUrl);
 
